b6df698db423cc1cd8d146efbcd3e54fcfdd498f
Thomas Stephens <tmsteph1290@gmail.com>
2025-11-24 18:36:36 -0800

    Fix prettier formatting for metadata and start icon

--- components/system/Taskbar/StartButton/StartButtonIcon.tsx
+++ components/system/Taskbar/StartButton/StartButtonIcon.tsx
@@ -21,7 +21,14 @@ const StartButtonIcon = memo(() => (
       </radialGradient>
     </defs>
     <rect fill="#050814" height="64" rx="14" width="64" />
-    <rect fill="url(#startInnerLight)" height="52" rx="12" width="52" x="6" y="6" />
+    <rect
+      fill="url(#startInnerLight)"
+      height="52"
+      rx="12"
+      width="52"
+      x="6"
+      y="6"
+    />
     <path
       d="M16 28.5c0-8.5 8.5-15.5 18.5-15.5S53 20 53 28.5c0 5.7-4 10.3-10.2 12.1l-2.9.8c-3.2.8-4.9 2-5.6 4.9l-.7 3.1h-6.7l1-4.9c1.2-5.5 5.2-7.9 9.5-9l2.8-.7c3-.8 4.8-2.7 4.8-5.4 0-4.1-4.2-7.6-10.3-7.6-5.8 0-10.9 3.3-10.9 8.2 0 1.6.7 3.6 1.8 5l-5.3 3.2C17.2 35.7 16 32.1 16 28.5Z"
       fill="url(#startPortalGlow)"
